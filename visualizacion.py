ba9055adf47a0319073fe1ae54b45a9bc8f1b8d0
Matias-Bustamante <matybustamante151@gmail.com>
2023-08-16 19:30:35 -0300

    Visualizacion terminada

--- visualizacion.py
+++ visualizacion.py
@@ -5,6 +5,8 @@ import plotly.express as px
 import io 
 from pathlib import Path 
 from api.clima import LoadAPIClima
+import os 
+import datetime 
 
 
 @st.cache_data
@@ -13,22 +15,32 @@ def convert_to_csv(data):
 
 
 if __name__=='__main__': 
-
+   
     
-    ##Retornamos una dashboard de dataframe de los datos del clima de los utlimos 5 dias 
-    data=pd.read_excel('clima.xlsx') 
+    st.sidebar.image('./logo.png', caption='Temperatura OpenWeatherMap')
     tab1, tab2, tab3, tab4, tab5, tab6=st.tabs(['Tabla de Datos', 'Temperatura Media','Visualizacion', 'Mapa', 
                                     'Busqueda', 'Descargar Archivo'])
     
+    ##Retornamos una dashboard de dataframe de los datos del clima de los utlimos 5 dias 
+    path=os.path.dirname(os.path.abspath(__file__)) 
+    fechaActual=datetime.datetime.now() 
+    fecha=str(fechaActual.day)+"-"+str(fechaActual.month)+"-"+str(fechaActual.year) 
+   
+    
+    file=path+ "\DataAnalysis"+"_"+fecha+"_clima.xlsx"
+    
+    
+    data=pd.read_excel("DataAnalysis_9-8-2023_clima.xlsx") 
+   
+
     tab1.header("Temperatura de los ultimos 5 dias ")
     tab1.subheader("Datos de Temperatura") 
     tab1.dataframe(data)
     ##Retornamos la temperatura media por ciudad y lo mostramos en un dashboard
     df_ciudad=data.groupby(by='ciudad', as_index=False)['Temperatura Actual'].mean()
     df_ciudad.rename(columns={"Temperatura Actual": "Temperatura media"}, inplace=True)
-    print(data)
     tab2.subheader("Temperatura media por Ciudad") 
-    tab2.dataframe(df_ciudad)
+    tab2.dataframe(df_ciudad.style.highlight_max(subset=['Temperatura media'],color='yellow',axis=0))
 
     ##Retornamos un grafico de pie donde se muestra la temperatura media y la ciudad
     grafico=px.pie(df_ciudad, 
@@ -45,9 +57,18 @@ if __name__=='__main__':
     tab4.map(mapa,zoom=2)
 
     ##Realiza la busqueda de la ciudad de interes y muestra las coordenadas geograficas
+    
+    
     select_city=tab5.selectbox("Selecciona una Ciudad:", ciudad)
     filtro=data[data['ciudad']==select_city] 
-    tab5.markdown(f"Ciudad {filtro.iloc[0]['ciudad']} Latitud: {filtro.iloc[0]['Latitud']} Longitud: {filtro.iloc[0]['Longitud']}")
+    with tab5: 
+        col1, col2, col3=st.columns(3)
+        col1.metric("Ciudad", filtro.iloc[0]['ciudad'])
+        col2.metric("Latitud", filtro.iloc[0]["Latitud"])
+        col3.metric("Longitud", filtro.iloc[0]["Longitud"])
+    
+   
+  
 
     ##Realiza la descarga de los datos en formato de Excel
     buffer=io.BytesIO() 
@@ -66,9 +87,10 @@ if __name__=='__main__':
     
     ##Realiza la descarga de los datos en forma JSON
     API_JSON=LoadAPIClima()
-    data_json=API_JSON.exportarAJSON(data)
+    ##data_json=API_JSON.exportarAJSON(data)
+    file=path+"\DataAnalysis"+"_"+fecha+"_clima.json"
     tab6.download_button(label='Descargar Archivo en forma JSON', 
-                         data=Path('clima.json').read_text(), 
+                         data=Path("DataAnalysis_9-8-2023_clima.json").read_text(), 
                          file_name='clima.json', 
                          mime='application/json')
                          
